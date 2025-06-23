bc8bb492fc3a7b80a89da689f437e7b2a5ac126e
Passakorn-max0360 <66011129@kmitl.ac.th>
2025-06-23 15:24:07 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,5 @@
+print("Hey, what's your first name? :")
+first_name = input().strip()
+print("And your last name? :")
+last_name = input().strip()
+print("Well, pleased to meet you,"+ " " + first_name + " " + last_name + ".")
