e42d2ae3ff4b39b1537c35d7d66a43f26dbfc2b5
Passakorn-max0360 <66011129@kmitl.ac.th>
2025-06-23 21:07:34 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+print("Give me the first number: ")
+first = int(input())
+print("Give me the second number: ")
+second = int(input())
+print("Thank you!")
+plus = first + second
+minus = first - second
+mult = first * second
+div = first / second
+print(f"{first} + {second} = {plus}")
+print(f"{first} - {second} = {minus}")
+print(f"{first} / {second} = {div}")
+print(f"{first} * {second} = {mult}")
+
