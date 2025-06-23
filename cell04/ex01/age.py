07d43b212f11a2fa805d4944c12036d01b37db4e
Passakorn-max0360 <66011129@kmitl.ac.th>
2025-06-23 20:56:59 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+print("Please tell me your age: ")
+age = input()
+print("You are currently " + age + " years old.")
+age10 = int(age) + 10
+age20 = int(age) + 20
+age30 = int(age) + 30
+print("In 10 years, you'll be " + str(age10) + " years old.")
+print(f"In 20 years, you'll be " + str(age20) + " years old.")
+print(f"In 30 years, you'll be " + str(age30) + " years old.")
