33180460b7e29ed7082364077c591a5f394fe4f1
Passakorn-max0360 <66011129@kmitl.ac.th>
2025-06-23 20:14:33 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("What you gotta say? : ")
+while True:
+    say = input()
+    if say == "STOP":
+        break
+    else:
+        print("I got that! Anything else? : ")
+        continue
