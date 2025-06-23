b2ba82971b5f111c1021d578fc5825a62f793fe2
Passakorn-max0360 <66011129@kmitl.ac.th>
2025-06-23 15:41:38 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+input_num = input()
+if input_num == "0":
+    print("This number is equal to zero.")
+if input_num != "0":
+    print("This number is different from zero.")
